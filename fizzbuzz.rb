15743c047c4f3d3486810cab6c97301af6b86679
KRIS DAVIS <krissdavis18@mail.com>
2019-06-04 00:31:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,25 +1,4 @@
-def "fizzbuzz" 
-  it  "Fizz" do
-     fizz_3 = fizzbuzz(3)
 
-    expect(fizz_3).to eq("Fizz")
-  end
-  it  "Buzz" do
-    fizz_5 = fizzbuzz(5)
-
-    expect(fizz_5).to eq("Buzz")
-  end
-  it "FizzBuzz"  do
-    fizz_15 = fizzbuzz(15)
-
-    expect(fizz_15).to eq("FizzBuzz")
-  end
-  it "nil" do
-    fizz_4 = fizzbuzz(4)
-
-    expect(fizz_4).to eq(nil)
-  end
-end
 
 
 def fizzbuzz(int)
